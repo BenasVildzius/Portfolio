85b4a008c881a67428529f9a6f80775ee3dba2ef
BenasVildzius <benassvildzius@gmail.com>
2024-08-07 03:31:28 +0300

    New logo, about me edit

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,27 +1,11 @@
-<<<<<<< HEAD
-=======
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaNpm, FaGithub, FaRobot } from 'react-icons/fa';
 import React from 'react';
 
->>>>>>> d1c438f (New logo, about me edit)
 export default function AboutMe(){
     return(
         <section id="AboutMe" className="about--section">
             
             <div className="hero--sectioni--content--box about--section--box">
-<<<<<<< HEAD
-                <div className="hero--section--content">
-                    <p className="section--title">About</p>
-                    <h1 className="skills--section--heading">About Me</h1>
-                    <p className="hero--section--description">
-                        I am a third-year student at Kauno Kolegija (Kaunas University of Applied Sciences), pursuing a bachelor's degree in Programming Systems/Software Engineering. I am from Lithuania and currently reside in Kaunas.
-                    </p>
-                    <p className="hero--section--description">
-                        Aside from my college work, I spend many hours on personal projects, researching various IT topics, and learning more about coding. I like to challenge myself and continuously strive to learn new things.
-                    </p>
-                    <p className="hero--section--description">
-                        I am currently seeking a job in IT as an intern or a junior employee. I am mostly interested in FullStack Development or Software Development. I am available to work full-time or part-time, depending on how it impacts my education.
-=======
                 <div className="hero--section--content--boutme">
                     <h1 className="skills--section--heading">About Me</h1>
                     <h3>Who am I?</h3>
@@ -56,7 +40,6 @@ export default function AboutMe(){
                         </li>
                     </ul>
                     Additionally, I've learned to use <span className="chatgpt">ChatGPT</span> <FaRobot className="chatgpt"/> to my advantage, utilizing its capabilities to enhance my coding and problem-solving processes. I also use <span className="github">GitHub</span> <FaGithub className="github"/> for version control, ensuring my projects are well-organized and collaborative.
->>>>>>> d1c438f (New logo, about me edit)
                     </p>
                 </div>
             </div>
--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -1,8 +1,5 @@
 import data from "../../data/index.json"
-<<<<<<< HEAD
-=======
 import FadeInSection from "../../FadeInSection"
->>>>>>> d1c438f (New logo, about me edit)
 
 //Takes the data from json file and displays it
 export default function MySkills(){
@@ -14,21 +11,6 @@ export default function MySkills(){
                     My Expertise
                 </h2>
             </div>
-<<<<<<< HEAD
-            <div className="skills--section--container">
-                {data?.skills?.map((item, index) => (
-                    <div key={index} className="skills--section--card">
-                        <div className="skills--section--img">
-                            <img src={item.src} alt="Product Chain"/>
-                        </div>
-                        <div className="skills--section--card--content">
-                            <h3 className="skills--section--title">{item.title}</h3>
-                            <p className="skills--section--description">{item.description}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-=======
                 <div className="skills--section--container">
                     {data?.skills?.map((item, index) => (
                         <FadeInSection>
@@ -44,7 +26,6 @@ export default function MySkills(){
                         </FadeInSection>
                     ))}
                 </div>
->>>>>>> d1c438f (New logo, about me edit)
         </section>
     )
 }
