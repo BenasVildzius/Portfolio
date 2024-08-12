63028861472dd90b0bcd13bca4671c31a6490502
BenasVildzius <benassvildzius@gmail.com>
2024-08-12 17:05:29 +0300

    Corrected about me section

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -14,8 +14,13 @@ export default function AboutMe(){
                     </p>
                     <h3>Experience</h3>
                     <p className="hero--section--description-boutme">
-                    Throughout my years of coding and learning, I've developed a strong passion for web development. I'm particularly fascinated by the intricacies of UI/UX design, as well as the challenge of implementing robust backend functionality to support my designs. My journey has been driven by a desire to create seamless, user-friendly web experiences that are both aesthetically pleasing and highly functional. <br /><br />
-                    <h4>Over time, I've honed my skills in:</h4>
+                        Throughout my years of coding and learning, I've developed a strong passion for web development. I'm particularly fascinated by the intricacies of UI/UX design, as well as the challenge of implementing robust backend functionality to support my designs. My journey has been driven by a desire to create seamless, user-friendly web experiences that are both aesthetically pleasing and highly functional. <br />Additionally, I've learned to use <span className="chatgpt">ChatGPT</span> <FaRobot className="chatgpt"/> to my advantage, utilizing its capabilities to enhance my coding and problem-solving processes. I also use <span className="github">GitHub</span> <FaGithub className="github"/> for version control, ensuring my projects are well-organized and collaborative.
+                    </p>
+                    
+                </div>
+            </div>
+            <div className="about--section--skills">
+            <h4>Over time, I've honed my skills in:</h4>
                     <ul>
                         <li>
                             <p><span className="html">HTML</span> <FaHtml5 className="html"/></p>
@@ -39,9 +44,6 @@ export default function AboutMe(){
                             <p><span className="npm">npm</span> <FaNpm className="npm"/></p>
                         </li>
                     </ul>
-                    Additionally, I've learned to use <span className="chatgpt">ChatGPT</span> <FaRobot className="chatgpt"/> to my advantage, utilizing its capabilities to enhance my coding and problem-solving processes. I also use <span className="github">GitHub</span> <FaGithub className="github"/> for version control, ensuring my projects are well-organized and collaborative.
-                    </p>
-                </div>
             </div>
         </section>
     )
